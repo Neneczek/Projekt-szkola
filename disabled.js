1ac5fe4d4b2f5cf0da4c5e119cffa8af0fe78dad
Mateusz <halomon643@gmail.com>
2024-04-30 13:45:46 +0200

    accessibility added'

--- disabled.js
+++ disabled.js
@@ -15,7 +15,7 @@ const disabledNoClose = () => {
 
 disabledBlock.addEventListener("click", disabledOpen);
 
-for (let i = 0; i < 4; i++) {
+for (let i = 0; i < 3; i++) {
   noClose[i].addEventListener("click", disabledNoClose);
 }
 
@@ -36,6 +36,56 @@ const invertNav = () => {
   buttons.classList.toggle("blackButtons");
   footer.classList.toggle("blackFooter");
   credits.classList.toggle("blackCredits");
+  disabled.classList.toggle("blackDisabled");
 };
 
 contrast.addEventListener("click", invertNav);
+
+const newHtml = document.querySelector("html");
+const fontPlus = document.querySelector(".fontPlus");
+const fontMinus = document.querySelector(".fontMinus");
+const reset = document.querySelector(".disabledImg");
+
+const fontSizePlus = () => {
+  //   newHtml.style.fontSize = "80%";
+  var wielkosc = parseInt(
+    window.getComputedStyle(newHtml, null).getPropertyValue("font-size")
+  );
+  if (wielkosc > 12) {
+    wielkosc = 12;
+    newHtml.style.fontSize = wielkosc + 1 + "px";
+  } else {
+    newHtml.style.fontSize = wielkosc + 1 + "px";
+  }
+};
+
+const fontSizeMinus = () => {
+  var wielkosc = parseInt(
+    window.getComputedStyle(newHtml, null).getPropertyValue("font-size")
+  );
+  if (wielkosc < 8) {
+    wielkosc = 8;
+    newHtml.style.fontSize = wielkosc + -1 + "px";
+  } else {
+    newHtml.style.fontSize = wielkosc + -1 + "px";
+  }
+};
+
+const resetAll = () => {
+  nav.classList.remove("navBlack");
+  schoolBlock.classList.remove("navBlack");
+  eduOffer.classList.remove("navBlack");
+  mobileNav.classList.remove("navBlack");
+  blockHeader.classList.remove("headerBlack");
+  buttons.classList.remove("blackButtons");
+  footer.classList.remove("blackFooter");
+  credits.classList.remove("blackCredits");
+  disabled.classList.remove("blackDisabled");
+  newHtml.style.fontSize = "62.5%";
+};
+
+fontPlus.addEventListener("click", fontSizePlus);
+
+fontMinus.addEventListener("click", fontSizeMinus);
+
+reset.addEventListener("click", resetAll);
